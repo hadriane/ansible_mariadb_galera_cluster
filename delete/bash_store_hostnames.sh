fd7f8ee1c4834101b411a6ccebc5529354f80c4a
hadriane <60728086+hadriane@users.noreply.github.com>
2020-02-29 20:46:35 +0800

    29.02.2020 Deleted delete folder

--- delete/bash_store_hostnames.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Loop through IP's files. For each IP get it's hostname
-IFS=$'\n'
-set -f
-for i in $(cat < "$1"/files/ips); do
-  host1="$(ssh ansible@$i "hostname")"
-  echo "$host1" >> "$1"/files/hostnames
-done
